7831cb6e6b25f44033f7164eaa8a2acb32c1bef7
MatthewLC <mateusluiztj@hotmail.com>
2021-12-01 17:54:32 -0300

    Finishing setup

--- app/src/androidTest/java/com/example/automationtestproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/automationtestproject/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.automationtestproject
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +21,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    private val stringToBeVerified: String = "Hello World!"
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.automationtestproject", appContext.packageName)
+    }
+
+    @Test
+    fun changeText_sameActivity() {
+
+        // Check that the text was changed.
+        onView(withId(R.id.main_label))
+            .check(matches(withText(stringToBeVerified)))
     }
 }
